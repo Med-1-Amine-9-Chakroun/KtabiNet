99977b02ba3fcd277f6d4a3409d8e999ddf5226b
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-02-22 22:08:36 +0100

    EditProfileClient: FormType, Interface EditProfile, Controller and data DataExtraction Completed

--- src/Controller/ClientsController.php
+++ src/Controller/ClientsController.php
@@ -2,18 +2,48 @@
 
 namespace App\Controller;
 
+use App\Form\ClientFormType;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientsController extends AbstractController
 {
+
+    private $em;
+    private $clientRepository;
+    // private $em;
+     public function __construct(ClientRepository $clientRepository, EntityManagerInterface $em)
+     {
+        $this->em =$em;
+        $this->clientRepository = $clientRepository;
+     }
+
     #[Route('/clients', name: 'app_clients')]
     public function index(): JsonResponse
     {
+        
+         
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ClientsController.php',
+        ]);
+    }
+
+
+    #[Route('/client/edit/{id}', name: 'app_client_edit')]
+    public function edit($id): Response
+    {
+        $client = $this->clientRepository->find($id);
+        $form = $this->createForm(ClientFormType::class, $client);
+        // dd($client);
+        return $this->render('Client/EditProfile.html.twig', [
+            'client' => $client,
+            'form' => $form->createView()
         ]);
     }
 }
