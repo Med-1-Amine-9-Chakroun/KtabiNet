f7a8954587dbd424679d235784735effba13529e
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-04-18 22:45:39 +0100

    Adding other charts

--- public/assets/Charts.js
+++ public/assets/Charts.js
@@ -1,9 +1,10 @@
 const ctx = document.getElementById('myChart');
 const ctx1 = document.getElementById('myChart1');
 const ctx2 = document.getElementById('myChart2');
-// const ctx3 = document.getElementById('myChart3');
+const ctx3 = document.getElementById('myChart3');
 const ctx4 = document.getElementById('myChart4');
 const ctx5 = document.getElementById('myChart5');
+const ctx6 = document.getElementById('myChart6');
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -18,8 +19,6 @@ function chart(element){
 /**************************** */
 /****FLOUS PAR MOIS */
 /**************************** */
-console.log(typeof element["flousPmois"]);
-console.log( element["flousPmois"]);
 let flous = element["flousPmois"];
 // Trier les clés de l'objet
 const clesTrie = Object.keys(flous).sort((a, b) => a - b);
@@ -30,25 +29,24 @@ console.log(floustableau);
 
 floustableau = floustableau.map(element => Object.values(element)[0]);
 
-console.log(floustableau);
+
 
 
 /**************************** */
 /****COMMANDE PAR MOIS */
 /**************************** */
-console.log(typeof element["cmdPmois"]);
-console.log( element["cmdPmois"]);
+
 let cpm = element["cmdPmois"];
 // Trier les clés de l'objet
 const clesTriecpm = Object.keys(cpm).sort((a, b) => a - b);
 
 // Créer un tableau de paires clé-valeur triées
-let cpmtableau = clesTrie.map(cle => ({ [cle]: cpm[cle] }));
-console.log(cpmtableau);
+let cpmtableau = clesTriecpm.map(cle => ({ [cle]: cpm[cle] }));
+
 
 cpmtableau = cpmtableau.map(element => Object.values(element)[0]);
 
-console.log(cpmtableau);
+
 
 
 /********************************************* */
@@ -74,11 +72,11 @@ nbrClient.innerText = element['client'];
 //************************** */
 //************************** */
 /*****CHART 4 */
-const labels4 = Object.keys(element['lpC']);
-const val4 = Object.values(element['lpC']);
-/*****CHART 5 */
-const labels5 = Object.keys(element['lrC']);
-const val5 = Object.values(element['lrC']);
+const labels5 = Object.keys(element['lpC']);
+const val5 = Object.values(element['lpC']);
+/*****CHART 6 */
+const labels6 = Object.keys(element['lrC']);
+const val6 = Object.values(element['lrC']);
 
 //************************** */
 //************************** */
@@ -93,7 +91,7 @@ const val5 = Object.values(element['lrC']);
     const data1 = {
       labels: labels,
       datasets: [{
-        label: 'Janvier',
+        label: 'Flous',
         data: floustableau,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
@@ -141,7 +139,7 @@ const val5 = Object.values(element['lrC']);
 const data2 = {
     labels: labels,
     datasets: [{
-      label: 'Janvier',
+      label: 'Nbr Commande',
       data: cpmtableau,
       backgroundColor: [
         'rgb(5, 44, 207, 0.2)',
@@ -182,36 +180,60 @@ const data2 = {
 /******************************************************************************************* */
 
 
-/*const data3 = {
-    labels: labels,
-    datasets: [{
-      label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
-    }]
-  };
+const data3 = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
 
 new Chart(ctx3, {
-    type: 'line',
-    data: data3,        
-  });
-*/
+  type: 'line',
+  data: data3,        
+});
 
 
 /******************************************************************************************* */
 /******************************************************************************************* */
-/* ***                               CHART 4                                             *** */
+/* ***                               CHART 4                                           *** */
 /******************************************************************************************* */
 /******************************************************************************************* */
 
 
 const data4 = {
-    labels: labels4,
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(201, 203, 207)',
+    tension: 0.1
+  }]
+};
+
+  new Chart(ctx4, {
+    type: 'line',
+    data: data4,        
+});
+
+
+
+/******************************************************************************************* */
+/******************************************************************************************* */
+/* ***                               CHART 5                                            *** */
+/******************************************************************************************* */
+/******************************************************************************************* */
+
+
+const data5 = {
+    labels: labels5,
     datasets: [{
       label: 'My First Dataset',
-      data: val4,
+      data: val5,
       backgroundColor: [
         'rgb(255, 99, 132)',
         'rgb(54, 162, 235)',
@@ -227,9 +249,9 @@ const data4 = {
     }]
   };
           
-    new Chart(ctx4, {
+    new Chart(ctx5, {
         type: 'doughnut',
-        data: data4,        
+        data: data5,        
       });
 
 
@@ -285,17 +307,17 @@ const data4 = {
       
 /******************************************************************************************* */
 /******************************************************************************************* */
-/* ***                               CHART 5                                             *** */
+/* ***                               CHART 6                                            *** */
 /******************************************************************************************* */
 /******************************************************************************************* */
 
 
 
-const data5 = {
-    labels: labels5,
+const data6 = {
+    labels: labels6,
     datasets: [{
       label: 'My First Dataset',
-      data: val5,
+      data: val6,
       backgroundColor: [
         'rgb(255, 99, 132)',
         'rgb(54, 162, 235)',
@@ -311,9 +333,9 @@ const data5 = {
     }]
   };
           
-    new Chart(ctx5, {
+    new Chart(ctx6, {
         type: 'doughnut',
-        data: data5,        
+        data: data6,        
       });
 
 
