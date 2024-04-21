1774dcdc1c9099023db19549ae5cfb70b1cbf08b
Amira <amiraboubakeresprims@gmail.com>
2024-04-21 19:00:16 +0200

    reajout AccesFixtures

--- src/DataFixtures/AccesFixtures.php
+++ src/DataFixtures/AccesFixtures.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App\DataFixtures;
-
 use App\Entity\Acces;
 use App\Entity\LivrePdf;
+use App\Entity\Client;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -14,25 +13,30 @@ class AccesFixtures extends Fixture
     {
         $faker = Factory::create();
 
-        // Load existing LivrePDF entities
+        // Load existing LivrePDF and Client entities
         $livres = $manager->getRepository(LivrePdf::class)->findAll();
+        $clients = $manager->getRepository(Client::class)->findAll();
 
-        // Ensure that there are LivrePdf entities available
-        if (empty($livres)) {
-            // If there are no LivrePdf entities, load LivrePdf fixtures first or create them programmatically
-            // Example: $this->load(LivrePdfFixtures::class);
+        // Ensure that there are LivrePdf and Client entities available
+        if (empty($livres) || empty($clients)) {
+            // Load LivrePdf and Client fixtures first or create them programmatically
+            // Example: $this->load(LivrePdfFixtures::class); $this->load(ClientFixtures::class);
             // Then retrieve them again
             $livres = $manager->getRepository(LivrePdf::class)->findAll();
+            $clients = $manager->getRepository(Client::class)->findAll();
         }
 
         // Generate sample data for Acces
         foreach ($livres as $livre) {
-            $acces = new Acces();
-            $acces->setDate($faker->dateTimeBetween('-1 year', 'now'));
-            $acces->setAcces($faker->boolean);
-            $acces->setIdLivrePdf($livre);
+            foreach ($clients as $client) {
+                $acces = new Acces();
+                $acces->setDate($faker->dateTimeBetween('-1 year', 'now'));
+                $acces->setAcces($faker->boolean);
+                $acces->setIdLivrePdf($livre);
+                $acces->setIdClient($client);
 
-            $manager->persist($acces);
+                $manager->persist($acces);
+            }
         }
 
         $manager->flush();
