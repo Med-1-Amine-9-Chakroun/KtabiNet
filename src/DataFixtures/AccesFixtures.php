982abf1f09699142491a63f1c7c0573293c00545
Amira <amiraboubakeresprims@gmail.com>
2024-04-21 12:59:41 +0200

    ajout de acces-admin page

--- src/DataFixtures/AccesFixtures.php
+++ src/DataFixtures/AccesFixtures.php
@@ -2,15 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Acces;
+use App\Entity\LivrePdf;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AccesFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        // Load existing LivrePDF entities
+        $livres = $manager->getRepository(LivrePdf::class)->findAll();
+
+        // Generate sample data for Acces
+        for ($i = 0; $i < 100; $i++) {
+            $acces = new Acces();
+            $acces->setDate($faker->dateTimeBetween('-1 year', 'now'));
+            $acces->setAcces($faker->boolean);
+
+            // Fetch a random LivrePDF entity only if $livres array is not empty
+            if (!empty($livres)) {
+                $livre = $livres[array_rand($livres)];
+                $acces->setIdLivrePdf($livre->getIdLivrePdf());
+                $manager->persist($acces);
+            }
+        }
 
         $manager->flush();
     }
---
+++ src/Form/AccesType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Acces;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Date')
+            ->add('Acces')
+            ->add('IdClient')
+            ->add('IdLivrePdf')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Acces::class,
+        ]);
+    }
+}
