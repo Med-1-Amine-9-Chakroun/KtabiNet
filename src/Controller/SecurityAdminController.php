5bc99f84fbacd995500283e1199149befb420161
Med Amine Chakroun <chakrounmedamine19@gmail.com>
2024-04-21 15:27:10 +0100

    Creating AddAdmin interface

--- src/Controller/SecurityAdminController.php
+++ src/Controller/SecurityAdminController.php
@@ -20,7 +20,7 @@ class SecurityAdminController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
+          
         return $this->render('admin\security\adminlogin.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
